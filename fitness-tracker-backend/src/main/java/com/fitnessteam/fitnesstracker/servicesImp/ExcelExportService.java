402ea5bacce30c6552cf6d29ece77bb2e0f9d928
replacool <alperarslan@eskisehir.edu.tr>
2023-01-01 22:05:02 +0300

    new Update

--- fitness-tracker-backend/src/main/java/com/fitnessteam/fitnesstracker/servicesImp/ExcelExportService.java
+++ fitness-tracker-backend/src/main/java/com/fitnessteam/fitnesstracker/servicesImp/ExcelExportService.java
@@ -14,12 +14,13 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 
 @Service
 public class ExcelExportService {
 
     private final MessageSource messageSource;
+
+    private static final String EXPORT_FAIL_ERR_MSG = "export.failed";
 
     @Autowired
     public ExcelExportService(MessageSource messageSource) {
@@ -27,12 +28,56 @@ public class ExcelExportService {
     }
 
     public ByteArrayInputStream exportTrainingProgram(List<TrainingProgramDto> trainingProgramDtoList){
+        try(Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream();){
+            Sheet sheet = workbook.createSheet("TrainingProgram");
+            String[] columns = {"MoveName",
+                    "RepetititonNumber",
+                    "SetNumber",
+                    "Target",
+                    "Feeling",
+                    "Reps",
+                    };
+
+            CellStyle style = workbook.createCellStyle();
+            XSSFFont font = (XSSFFont) workbook.createFont();
+            font.setBold(true);
+            font.setFontHeight(20);
+
+            style.setFont(font);
+            style.setAlignment(HorizontalAlignment.CENTER);
+
+            Row headerRow = sheet.createRow(0);
 
 
-        return null;
+            font.setFontHeight(10);
 
+            for (int col = 0; col < columns.length; col++) {
+                Cell cell = headerRow.createCell(col);
+                cell.setCellStyle(style);
+                cell.setCellValue(messageSource.getMessage(columns[col],
+                        null, LocaleContextHolder.getLocale()));
+            }
+
+            int rowIdx = 1;
+            for (TrainingProgramDto trainingProgramDto: trainingProgramDtoList){
+                Row row = sheet.createRow(rowIdx++);
+
+                row.createCell(0).setCellValue(trainingProgramDto.getName());
+                row.createCell(0).setCellValue(trainingProgramDto.getRepetitionNumber());
+                row.createCell(0).setCellValue(trainingProgramDto.getSetNumber());
+                row.createCell(0).setCellValue(trainingProgramDto.getTarget());
+            }
+
+            for (int i = 0; i < columns.length; i++) {
+                sheet.autoSizeColumn(i);
+            }
+
+            workbook.write(out);
+            return new ByteArrayInputStream(out.toByteArray());
+
+        } catch (IOException e) {
+            throw new RuntimeException(EXPORT_FAIL_ERR_MSG);
+        }
     }
-
-
 
 }
